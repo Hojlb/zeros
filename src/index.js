5565dd2fac6cf8a492c1691102f6b8d46282e666
Maksim Palonski <mchp010nsky@gmail.com>
2018-02-25 02:55:07 +0300

    solver

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+    let count = 1;
+
+    let num = number;
+    let divider = 5;
+    let result = number;
+
+    while ( result >= 10 )
+    {
+        result = (num - num % divider) / divider;
+        divider = divider * 5;
+        count += result;
+    }
+
+    if( result < 5 && result >= 2 ){
+        count--;
+    }
+
+    return( count );
+
 }
